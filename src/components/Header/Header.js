818567eb3f79b3d26a345ffb68ae1a47aa1befa0
ahn-sujin <asj9674@gmail.com>
2022-09-19 21:24:18 +0900

    Add:header 레이아웃 및 스타일 작업 완료

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="logo">정육쩜</h1>
+        <div className="menu_wrap">
+          <ul className="service_menu">
+            <li>쇼핑하기</li>
+            <li>배송안내</li>
+            <li>이벤트</li>
+          </ul>
+          <div className="etc_menu">
+            <ul className="contact_menu">
+              <li>공지사항</li>
+              <li>고객센터</li>
+            </ul>
+            <ul className="register_menu">
+              <li>로그인</li>
+              <li>회원가입</li>
+            </ul>
+            <p className="cart">
+              <i className="fa-solid fa-cart-shopping" />
+            </p>
+            <button className="all_menu_btn">
+              <i className="fa-solid fa-bars" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
