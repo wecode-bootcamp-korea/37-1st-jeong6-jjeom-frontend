4dd48ecb2e2070a819431a54645a263508569723
Shin Ju An <shinjuan0101@gmail.com>
2022-09-21 14:13:42 +0900

    Modify: PR 리뷰 반영

--- src/pages/List/Products.js
+++ src/pages/List/Products.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Products.scss';
+
 const Products = ({ data }) => {
   const { title, price, gram, img_url, antibiotic } = data;
   return (
     <li className="list_data_container">
       <div className="list_img">
-        {antibiotic && <span>무항생제</span>}
+        {antibiotic && <span className="">무항생제</span>}
         <img src={img_url} alt="상품 이미지" />
         <button>
           <i className="fa-solid fa-cart-shopping" />
