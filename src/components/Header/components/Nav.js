8c7d0f16d0f519062187dca0cea0e9b0a81c54ac
ahn-sujin <asj9674@gmail.com>
2022-09-21 15:15:42 +0900

    Modify: header 수정사항 반영

--- src/components/Header/components/Nav.js
+++ src/components/Header/components/Nav.js
@@ -3,9 +3,9 @@ import './Nav.scss';
 import NAV_LIST from '../navData';
 const Nav = ({ isActive, showNav }) => {
   return (
-    <nav className={isActive ? 'nav active' : 'nav'}>
-      <div className="dim" onClick={showNav} />
-      <div className="nav_box">
+    <>
+      <div className={`dim ${isActive ? ' active' : ''}`} onClick={showNav} />
+      <div className={`nav_box ${isActive ? ' active' : ''}`}>
         <section className="menu_top">
           <h3 className="menu_name">메뉴</h3>
           <ul className="icon_menu">
@@ -20,40 +20,40 @@ const Nav = ({ isActive, showNav }) => {
         <section className="menu_list">
           <h4 className="menu_name">쇼핑하기</h4>
           <ul className="catogry_list">
-            {NAV_LIST.map(data => (
-              <li key={data.id}>
-                <img src={data.image} alt={data.catogory} />
-                <p className="category_name">{data.catogory}</p>
+            {NAV_LIST.map(({ id, image, catogory }) => (
+              <li key={id}>
+                <img src={image} alt={catogory} />
+                <p className="category_name">{catogory}</p>
               </li>
             ))}
           </ul>
         </section>
         <section className="menu_link">
-          <p>브랜드스토리</p>
-          <p>이용가이드</p>
-          <p>공지사항</p>
-          <p>고객센터</p>
+          <p className="menu_name">브랜드스토리</p>
+          <p className="menu_name">이용가이드</p>
+          <p className="menu_name">공지사항</p>
+          <p className="menu_name">고객센터</p>
         </section>
         <section className="menu_member">
-          <p className="mypage hide">
+          <p className="menu_name hide">
             <i className="fa-regular fa-user" />
             마이페이지
           </p>
-          <p className="logout hide">
+          <p className="menu_name hide">
             <i className="fa-solid fa-arrow-right-from-bracket" />
             로그아웃
           </p>
-          <p className="signup">
+          <p className="menu_name">
             <i className="fa-solid fa-user-plus" />
             회원가입
           </p>
-          <p className="login">
+          <p className="menu_name">
             <i className="fa-solid fa-arrow-right-to-bracket" />
             로그인
           </p>
         </section>
       </div>
-    </nav>
+    </>
   );
 };
 
