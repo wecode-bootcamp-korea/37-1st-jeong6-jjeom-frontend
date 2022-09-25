3766cf0dad08949a6f4a35309ffceaa9d735e296
ahn-sujin <asj9674@gmail.com>
2022-09-25 18:40:10 +0900

    Add:장바구니에 담긴 상품 없을 떄 ItemNone 컴포넌트 보여지도록 기능 구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -84,69 +84,74 @@ const Cart = () => {
     <div className="cart">
       <h3 className="title">장바구니</h3>
       <div className="container">
-        <div className="item_table">
-          <div className="item_header">
-            <div className="check_area">
-              <input
-                type="checkbox"
-                id="checkAll"
-                title="선택"
-                checked={isAllCheckBox}
-                onChange={e => {
-                  handelAllCheckbox(e.target.checked);
+        {cartItem.length === 0 ? (
+          <ItemNone />
+        ) : (
+          <>
+            <div className="item_table">
+              <div className="item_header">
+                <div className="check_area">
+                  <input
+                    type="checkbox"
+                    id="checkAll"
+                    title="선택"
+                    checked={isAllCheckBox}
+                    onChange={e => {
+                      handelAllCheckbox(e.target.checked);
+                    }}
+                  />
+                  <label htmlFor="checkAll" />
+                </div>
+                <p className="header_title">상품정보</p>
+                <p className="header_title">수량</p>
+                <p className="header_title">가격</p>
+              </div>
+              <ul className="item_list">
+                {cartItem.map(data => (
+                  <CartItemList
+                    key={data.id}
+                    itemInfo={data}
+                    cartItem={cartItem}
+                    setCartItem={setCartItem}
+                    onChangeProps={onChangeProps}
+                  />
+                ))}
+              </ul>
+              <button
+                className="all_delete"
+                onClick={() => {
+                  removeChild(checkedId);
                 }}
-              />
-              <label htmlFor="checkAll" />
+              >
+                선택 상품 삭제
+              </button>
             </div>
-            <p className="header_title">상품정보</p>
-            <p className="header_title">수량</p>
-            <p className="header_title">가격</p>
-          </div>
-          <ul className="item_list">
-            {cartItem.map(data => (
-              <CartItemList
-                key={data.id}
-                itemInfo={data}
-                cartItem={cartItem}
-                setCartItem={setCartItem}
-                onChangeProps={onChangeProps}
-              />
-            ))}
-          </ul>
-          <button
-            className="all_delete"
-            onClick={() => {
-              removeChild(checkedId);
-            }}
-          >
-            선택 상품 삭제
-          </button>
-        </div>
-        <div className="payment_area">
-          <ul className="payment_list">
-            <li className="all_price">
-              <p>총 상품 금액</p>
-              <p>{sumItemPrice()}원</p>
-            </li>
-            <li className="all_price">
-              <p>총 배송비</p>
-              <p>{checkItem.length === 0 ? '0원' : '3500원'}</p>
-            </li>
-            <li className="option_price">
-              <dl className="option">
-                <dt className="delivery">기본 배송비</dt>
-                <dd className="delivery">3500원</dd>
-              </dl>
-            </li>
-            <li>
-              <p className="final_title">예상 결제 금액</p>
-              <p className="final_price">{sumAllPrice(sumItemPrice)}원</p>
-            </li>
-          </ul>
-          <button className="payment_btn order">상품 주문하기</button>
-          <button className="payment_btn shopping">쇼핑계속하기</button>
-        </div>
-        <ItemNone />
+            <div className="payment_area">
+              <ul className="payment_list">
+                <li className="all_price">
+                  <p>총 상품 금액</p>
+                  <p>{sumItemPrice()}원</p>
+                </li>
+                <li className="all_price">
+                  <p>총 배송비</p>
+                  <p>{checkItem.length === 0 ? '0원' : '3500원'}</p>
+                </li>
+                <li className="option_price">
+                  <dl className="option">
+                    <dt className="delivery">기본 배송비</dt>
+                    <dd className="delivery">3500원</dd>
+                  </dl>
+                </li>
+                <li>
+                  <p className="final_title">예상 결제 금액</p>
+                  <p className="final_price">{sumAllPrice(sumItemPrice)}원</p>
+                </li>
+              </ul>
+              <button className="payment_btn order">상품 주문하기</button>
+              <button className="payment_btn shopping">쇼핑계속하기</button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
