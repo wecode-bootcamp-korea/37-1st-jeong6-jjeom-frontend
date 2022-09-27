78a165f3f84888237175a97620a23f2352c82d80
hyozzi92 <gytjdzja@gmail.com>
2022-09-27 21:01:29 +0900

    Add : sorting 기능 구현 완료

--- src/pages/main/BestItems.js
+++ src/pages/main/BestItems.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Products from '../List/Product';
 import './BestItems.scss';
-
+import ModalPortal from '../../Portal';
 import SortingModal from './SortingModal';
 
 const BestItems = () => {
@@ -10,19 +10,94 @@ const BestItems = () => {
 
   const [modal, setModal] = useState(false);
 
+  const [filtered, setFiltered] = useState([]);
+
+  const antiBio = bestItems.filter(e => {
+    return e.antibiotic == true;
+  });
+
+  let i = 0;
+
+  const tabClickHandler = index => {
+    setActiveIndex(index);
+  };
+
   const modalHandler = () => {
     setModal(true);
   };
 
-  const [searchParams, setSearchParams] = useSearchParams();
+  const arr = [];
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * bestItems.length);
+    let recommend = bestItems[randomIndex];
+    arr.push(recommend);
+  }
 
-  const getNames = searchParams.getAll('name');
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const tabContArr = [
+    {
+      tabTitle: (
+        <span
+          className={
+            activeIndex === 0
+              ? 'headerTitle is-active'
+              : 'headerTitle none-active'
+          }
+          onClick={() => {
+            tabClickHandler(0);
+          }}
+        >
+          무항생제
+        </span>
+      ),
+      tabCont: antiBio.map(data => {
+        return <Products data={data} key={data.id} />;
+      }),
+    },
+
+    {
+      tabTitle: (
+        <span
+          className={
+            activeIndex === 1
+              ? 'headerTitle is-active'
+              : 'headerTitle none-active'
+          }
+          onClick={() => {
+            tabClickHandler(1);
+          }}
+        >
+          베스트아이템
+        </span>
+      ),
+      tabCont: bestItems.map(data => {
+        return <Products data={data} key={data.id} />;
+      }),
+    },
+
+    {
+      tabTitle: (
+        <span
+          className={activeIndex === 2 ? 'is-active' : 'none-active'}
+          onClick={() => {
+            tabClickHandler(2);
+          }}
+        >
+          추천제품
+        </span>
+      ),
+      tabCont: arr.map((data, index) => {
+        return <Products data={data} key={index} />;
+      }),
+    },
+  ];
 
   useEffect(() => {
-    fetch(`./data/bestItems.json?name=${getNames}`)
+    fetch('./data/bestItems.json')
       .then(res => res.json())
       .then(result => setBestItems(result));
-  }, [getNames]);
+  }, []);
   return (
     <div className="bestItems">
       <div
@@ -31,16 +106,16 @@ const BestItems = () => {
         }}
         className="best_header"
       >
-        <span className="best_title">정육쩜 베스트 상품</span>
-        {modal && <SortingModal />}
+        <span className="best_title">
+          {tabContArr.map((section, index) => {
+            return section.tabTitle;
+          })}
+        </span>
+        {modal && <SortingModal bestItems={bestItems} />}
         <i onMouseOver={modalHandler} className="fa-solid fa-bars"></i>
       </div>
 
-      <ul className="products_list">
-        {bestItems.map(data => {
-          return <Products data={data} key={data.id} />;
-        })}
-      </ul>
+      <ul className="products_list">{tabContArr[activeIndex].tabCont}</ul>
     </div>
   );
 };
--- src/pages/main/SortingModal.js
+++ src/pages/main/SortingModal.js
@@ -1,39 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import Products from '../List/Product';
 import './SortingModal.scss';
 
-const SortingModal = ({}) => {
-  const [searchParams, setSearchParams] = useSearchParams();
+const SortingModal = ({ bestItems, filterAnti, key }) => {
   return (
     <div>
       <div className="sorting_wrap">
-        <button
-          onClick={() => {
-            searchParams.set('name', 'best_items');
-            setSearchParams(searchParams);
-          }}
-          className="sorting_best"
-        >
-          베스트상품
-        </button>
-        <button
-          onClick={() => {
-            searchParams.set('name', 'anti_bio');
-            setSearchParams(searchParams);
-          }}
-          className="sorting_antibiotic"
-        >
+        <button className="sorting_best">베스트상품</button>
+        <button onClick={filterAnti} className="sorting_antibiotic">
           무항생제
         </button>
-        <button
-          onClick={() => {
-            searchParams.set('name', 'recommend');
-            setSearchParams(searchParams);
-          }}
-          className="sorting_random"
-        >
-          추천상품
-        </button>
+        <button className="sorting_random">추천상품</button>
       </div>
     </div>
   );
