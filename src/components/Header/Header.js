430454619c1ec53b601fbb59fa44c9040d8af4ee
ahn-sujin <asj9674@gmail.com>
2022-09-27 11:39:50 +0900

    Add:header 메뉴 링크 연결

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     <>
       <header className="header">
         <div className="container">
-          <h1 className="logo" onClick={() => navigate('/')}>
+          <h1 className="logo" onClick={() => navigate('/main')}>
             <img src="./images/logo_w.png" alt="정육쩜 로고" />
           </h1>
           <div className="menu_wrap">
@@ -31,12 +31,27 @@ const Header = () => {
                 <li>고객센터</li>
               </ul>
               <ul className="register_menu">
-                <li className={token ? 'hide' : ''}>로그인</li>
-                <li className={token ? 'hide' : ''}>회원가입</li>
+                <li
+                  className={token ? 'hide' : ''}
+                  onClick={() => navigate('/login')}
+                >
+                  로그인
+                </li>
+                <li
+                  className={token ? 'hide' : ''}
+                  onClick={() => navigate('/signup')}
+                >
+                  회원가입
+                </li>
                 <li className={token ? '' : 'hide'}>로그아웃</li>
                 <li className={token ? '' : 'hide'}>마이페이지</li>
               </ul>
-              <p className="cart">
+              <p
+                className="cart"
+                onClick={() => {
+                  navigate('/cart');
+                }}
+              >
                 <i className="fa-solid fa-cart-shopping" />
               </p>
               <button className="all_menu_btn" onClick={showNav}>
--- src/components/Header/navData.js
+++ src/components/Header/navData.js
@@ -3,42 +3,49 @@ const NAV_LIST = [
     id: 1,
     image: './images/pig.png',
     catogory: '돼지',
-    path: '',
+    path: '/list?name=pork',
   },
   {
     id: 2,
     image: './images/cow.png',
     catogory: '소',
+    path: '/list?name=cow',
   },
   {
     id: 3,
     image: './images/chicken.png',
     catogory: '닭',
+    path: '/list?name=chicken',
   },
   {
     id: 4,
     image: './images/fish.png',
     catogory: '수산',
+    path: '/list?name=seafood',
   },
   {
     id: 5,
     image: './images/cutlery.png',
     catogory: '밀키트',
+    path: '/list?name=mealkit',
   },
   {
     id: 6,
     image: './images/milk.png',
     catogory: '우유',
+    path: '/list?name=milk',
   },
   {
     id: 7,
     image: './images/eggs.png',
     catogory: '계란',
+    path: '/list?name=egg',
   },
   {
     id: 8,
     image: './images/baby-food.png',
     catogory: '이유식',
+    path: '/list?name=baby',
   },
 ];
 
